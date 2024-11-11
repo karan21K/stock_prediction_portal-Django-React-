39ba8e7135f699b0119a9bcf8a312be76a1ca859
karan <karanbose1999@gmail.com>
2024-11-11 18:34:19 +0530

    integrating ML model with dj & rjs application

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accounts import views as UserViews
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import StockPredictionAPIView
 
 urlpatterns = [
     path('register/', UserViews.RegisterView.as_view()),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('protected-view/', UserViews.ProtectedView.as_view()),
+    
+    #prediction Api
+    path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction')
 ]
